a36d51e79970340c8a6c9c32168528fab692e78e
ZACkTHeFat <zackaryezra@gmail.com>
2019-06-03 20:35:27 -0700

    I styled the linker.html file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from emailer import send_email
 from urllib import parse
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 import os
 
 app = Flask(__name__)
@@ -17,6 +17,36 @@ def email():
 	)
 	return "An email has been sent (I hope)"
 
+@app.route("/send_something/<user_email>")
+def send_something(user_email):
+  subject = f"Hello, {user_email}"
+  message = """
+    Hi, {username},
+
+
+    Click <a href="http://127.0.0.1:33507/">this link</a> to return to our website.
+  """
+  message = message.format(username=user_email)
+  send_email(
+             user_email, message, subject)
+  return f"OKAY - email sent to {user_email}"
+
+@app.route("/send_to/<user_email>")
+def JOHN_CENA(user_email):
+  subject = f"Hello, {user_email}"
+  message = ("Hello M8 :)")
+
+
+  message = message.format(username=user_email)
+  send_email(
+             user_email, message, subject)
+  return f"OKAY - email sent to {user_email}"
+  
+@app.route("/contact")
+def name_response():
+    return render_template('linker.html')
+
+
 @app.route("/minecraft")
 def minecraft():
 	to_address = "zacke69420@gmail.com"
@@ -28,6 +58,7 @@ def minecraft():
 	subject
 	)
 	return "CHECK UR EMAIL TO SEE WHO THE BEST MINECRAFT CHARACTER IS (IT'S {})".format(os.environ.get('best_minecraft_character'))
+
 
 
 @app.route("/")
